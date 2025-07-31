510cc32a221f2ec618cc3c7b75d16aaedfba072e
Dell <Dell@DESKTOP-PU60M23>
2025-07-31 22:30:11 +0530

    I have added day one package

---
+++ corejavaprograms/src/com/tcf/daytwo/ContinueDemo.java
@@ -0,0 +1,13 @@
+package com.tcf.daytwo;
+
+public class ContinueDemo {
+
+	public static void main(String[] args) {
+		for (int i = 5; i < 10; i++) {
+			if (i == 5)
+			break;
+			System.out.println(i);
+		}
+
+	}
+}
